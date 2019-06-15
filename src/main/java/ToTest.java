a34028ef4527ef608e732bc6e3be8de36c0b1cb9
Yash Sampat <yasket@gmail.com>
2019-06-15 18:47:19 +0530

    improved the logic

--- src/main/java/ToTest.java
+++ src/main/java/ToTest.java
@@ -3,11 +3,13 @@ public class ToTest {
     public String removeA(String args) {
         String ans = "";
         char[] arr = args.toCharArray();
+        int c = 0;
         for(char a: arr ) {
-            if(a == 'A') {
+            if(a == 'A' && c<2) {
                 continue;
             }
             ans += a;
+            c++;
         }
         return ans;
     }
